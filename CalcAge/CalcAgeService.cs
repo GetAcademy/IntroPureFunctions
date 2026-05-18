5e522c3513a4e8208d9f004921ed01b7a9a87ab2
GET Academy <terje@kolderup.net>
2026-05-18 09:22:07 +0200

    Initial commit

---
+++ CalcAge/CalcAgeService.cs
@@ -0,0 +1,36 @@
+﻿namespace CalcAge
+{
+    internal class CalcAgeService
+    {
+        public static int RunPure(int birthYear, bool hasHadBirthday, int currentYear)
+        {
+            var age = currentYear - birthYear;
+
+            if (!hasHadBirthday)
+            {
+                age--;
+            }
+
+            return age;
+        }
+
+        public static void RunImpure()
+        {
+            Console.Write("Fødselsår: ");
+            var birthYear = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Har du hatt bursdag i år? j/n: ");
+            var hasHadBirthday = Console.ReadLine()! == "j";
+
+            var currentYear = DateTime.Now.Year;
+            var age = currentYear - birthYear;
+
+            if (!hasHadBirthday)
+            {
+                age--;
+            }
+
+            Console.WriteLine($"Du er {age} år.");
+        }
+    }
+}
---
+++ CalcAge/Program.cs
@@ -0,0 +1,13 @@
+﻿using CalcAge;
+
+//CalcAgeService.RunImpure();
+
+Console.Write("Fødselsår: ");
+var birthYear = int.Parse(Console.ReadLine()!);
+
+Console.Write("Har du hatt bursdag i år? j/n: ");
+var hasHadBirthday = Console.ReadLine()! == "j";
+
+var age = CalcAgeService.RunPure(birthYear, hasHadBirthday);
+
+Console.WriteLine($"Du er {age} år.");
